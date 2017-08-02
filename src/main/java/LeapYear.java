d388ba106c800c8e8fa17c5e74a05efe9d35b4d6
dennismuturia <dmuturia2922@gmail.com>
2017-08-02 10:54:10 +0300

    Made a build successfully

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,6 +1,8 @@
 public class LeapYear{
   public boolean isLeapYear(int year){
-    if (year%100 == 0) {
+    if (year%400 == 0) {
+      return true;
+    }else if (year%100 == 0) {
       return false;
     }else{
       return year%4 == 0;
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -11,6 +11,12 @@ public class LeapYearTest{
   @Test
   public void isLeapYear_forDivisibleByHundred_true(){
     LeapYear leapyear = new LeapYear();
-    assertEquals(false, leapyear.isLeapYear(2000));
+    assertEquals(false, leapyear.isLeapYear(1900));
+  }
+
+  @Test
+  public void isLeapYear_forDivisibleByFourHundred_true(){
+    LeapYear leapyear = new LeapYear();
+    assertEquals(true, leapyear.isLeapYear(2000));
   }
 }
