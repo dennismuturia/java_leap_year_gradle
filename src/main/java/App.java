56404ecebfee6c21075351aaf88bea69d52e092f
dennismuturia <dmuturia2922@gmail.com>
2017-08-02 11:11:32 +0300

    Successfully compiledthe programe

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,15 @@ import java.io.Console;
 
 public class App{
   public static void main(String []args){
-    
+    Console myConsole = System.console();
+    System.out.println("Enter the year you wanna test to seeif its a leap year");
+    String myYear = myConsole.readLine();
+
+    int theYear = Integer.parseInt(myYear);
+
+    LeapYear leapyear = new LeapYear();
+    boolean leapyearResult = leapyear.isLeapYear(theYear);
+
+    System.out.println("Is that a leap year? " +leapyearResult);
   }
 }
